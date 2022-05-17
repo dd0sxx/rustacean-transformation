96b4b004f6c86be58c895be2386f7cca5219d477
dd0sxx <contacttheotelonis@gmail.com>
2022-05-17 00:07:35 -0400

    recursive fibonacci

--- src/n_fibonacci/src/main.rs
+++ src/n_fibonacci/src/main.rs
@@ -1,24 +1,15 @@
 fn main() {
     let mut x = 0;
     while x < 12 {
-        let a = n_fibonacci(x);
+        let a = recursive_fibonacci(x);
         println!("{} Fibonacci number is {}", x, a);
         x += 1;
     }
 }
 
-fn n_fibonacci (n: u128) -> u128 {
-    let mut f = 0;
-    let mut previous = 0;
-    let mut counter = 0;
-    while counter <= n {
-        if counter == 1 {
-            f = 1;
-        }
-        let nextPrevious = f;
-        f = f + previous;
-        previous = nextPrevious;
-        counter += 1;
-    }
-    f
+fn recursive_fibonacci (n: u128) -> u128 {
+    if n <= 1 {
+        return n
+    } 
+    return recursive_fibonacci(n-1) + recursive_fibonacci(n-2);
 }
