c38c881ac960e3946f122f939cd4bc42c866d43f
dd0sxx <contacttheotelonis@gmail.com>
2022-05-16 22:03:13 -0400

    conditionals

---
+++ src/conditionals/target/debug/conditionals.d
@@ -0,0 +1,1 @@
+/Users/theo/Documents/rustacean-transformation/src/conditionals/target/debug/conditionals: /Users/theo/Documents/rustacean-transformation/src/conditionals/src/main.rs
---
+++ src/conditionals/target/debug/deps/conditionals-20476e0505e16f03.d
@@ -0,0 +1,5 @@
+/Users/theo/Documents/rustacean-transformation/src/conditionals/target/debug/deps/conditionals-20476e0505e16f03: src/main.rs
+
+/Users/theo/Documents/rustacean-transformation/src/conditionals/target/debug/deps/conditionals-20476e0505e16f03.d: src/main.rs
+
+src/main.rs:
