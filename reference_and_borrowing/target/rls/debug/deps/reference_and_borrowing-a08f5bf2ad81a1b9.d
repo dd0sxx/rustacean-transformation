8f793c9127f76ce2733faa64cb5376d9247377ce
dd0sxx <contacttheotelonis@gmail.com>
2022-05-22 11:40:42 -0400

    references

---
+++ reference_and_borrowing/target/rls/debug/deps/reference_and_borrowing-a08f5bf2ad81a1b9.d
@@ -0,0 +1,5 @@
+/Users/theo/Documents/rustacean-transformation/reference_and_borrowing/target/rls/debug/deps/reference_and_borrowing-a08f5bf2ad81a1b9.rmeta: src/main.rs
+
+/Users/theo/Documents/rustacean-transformation/reference_and_borrowing/target/rls/debug/deps/reference_and_borrowing-a08f5bf2ad81a1b9.d: src/main.rs
+
+src/main.rs:
---
+++ reference_and_borrowing/target/rls/debug/deps/reference_and_borrowing-b9d356899b8fabae.d
@@ -0,0 +1,5 @@
+/Users/theo/Documents/rustacean-transformation/reference_and_borrowing/target/rls/debug/deps/reference_and_borrowing-b9d356899b8fabae.rmeta: src/main.rs
+
+/Users/theo/Documents/rustacean-transformation/reference_and_borrowing/target/rls/debug/deps/reference_and_borrowing-b9d356899b8fabae.d: src/main.rs
+
+src/main.rs:
