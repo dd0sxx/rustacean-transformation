e8e5bde08578c8afe33e90f6fe7bda7b3317a404
dd0sxx <contacttheotelonis@gmail.com>
2022-05-22 12:33:59 -0400

    ref

--- reference_and_borrowing/src/main.rs
+++ reference_and_borrowing/src/main.rs
@@ -4,8 +4,31 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}.", s1, len);
+    
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
 }
+
+fn mut_and_immut_references () {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+}
+
+// At any given time, you can have either one mutable reference or any number of immutable references.
+// References must always be valid.
