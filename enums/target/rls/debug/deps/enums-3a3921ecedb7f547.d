14df2702bbe2da4ea150957b52925fb270cb65cc
dd0sxx <contacttheotelonis@gmail.com>
2022-06-11 20:19:19 +0100

    enums

---
+++ enums/target/rls/debug/deps/enums-3a3921ecedb7f547.d
@@ -0,0 +1,5 @@
+/Users/theo/Documents/rustacean-transformation/enums/target/rls/debug/deps/enums-3a3921ecedb7f547.rmeta: src/main.rs
+
+/Users/theo/Documents/rustacean-transformation/enums/target/rls/debug/deps/enums-3a3921ecedb7f547.d: src/main.rs
+
+src/main.rs:
---
+++ match-ct-flow/target/rls/debug/deps/match_ct_flow-19548c4c2867bde5.d
@@ -0,0 +1,5 @@
+/Users/theo/Documents/rustacean-transformation/match-ct-flow/target/rls/debug/deps/match_ct_flow-19548c4c2867bde5.rmeta: src/main.rs
+
+/Users/theo/Documents/rustacean-transformation/match-ct-flow/target/rls/debug/deps/match_ct_flow-19548c4c2867bde5.d: src/main.rs
+
+src/main.rs:
