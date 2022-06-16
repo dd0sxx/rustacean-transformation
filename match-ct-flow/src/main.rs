febfe1d04be78274208a49c16a982d94ccbe9a70
dd0sxx <contacttheotelonis@gmail.com>
2022-06-16 20:19:19 +0100

    _ placeholder

--- match-ct-flow/src/main.rs
+++ match-ct-flow/src/main.rs
@@ -34,7 +34,9 @@ fn plus_one (x: Option<i32>) -> Option<i32> {
     }
 }
 
-
+fn add_fancy_hat () {}
+fn remove_fancy_hat () {}
+fn reroll () {}
 
 
 
@@ -42,4 +44,11 @@ fn main() {
     let five = Some(5);
     let six = plus_one(five);
     let none = plus_one(None);
+    
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => reroll(),
+    }
 }
